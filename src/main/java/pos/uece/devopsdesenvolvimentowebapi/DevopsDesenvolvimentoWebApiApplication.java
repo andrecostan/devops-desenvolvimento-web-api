06e95771c74ed724b1b6b1841dd40dc14767a78e
André Costa <andrecostan@gmail.com>
2024-03-09 11:34:04 -0300

    first commit

---
+++ src/main/java/pos/uece/devopsdesenvolvimentowebapi/DevopsDesenvolvimentoWebApiApplication.java
@@ -0,0 +1,13 @@
+package pos.uece.devopsdesenvolvimentowebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDesenvolvimentoWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDesenvolvimentoWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pos/uece/devopsdesenvolvimentowebapi/DevopsDesenvolvimentoWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pos.uece.devopsdesenvolvimentowebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDesenvolvimentoWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
